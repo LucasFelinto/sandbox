79446d33448431225efdef66b264f573305304e1
LucasFelinto <elucasfelinto@gmail.com>
2020-03-06 14:21:34 -0300

    GH-14 Verificação de matricula e siape

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -3,13 +3,26 @@ import User from '../schemas/User';
 class UserController {
   async store(req, res) {
     const { type } = req.params;
-
+    const { matricula, siape } = req.body;
+    
     const users = {
       async student() {
+        const studentExists = await User.Aluno.findOne({ matricula });
+        
+        if (studentExists) {
+          return res.status(400).json({ error: "Matricula ja cadastrada" });
+        }
+        
         const student = await User.Aluno.create(req.body);
         return student;
       },
       async employee() {
+        const employeeExists = await User.Funcionario.findOne({ siape });
+
+        if (employeeExists) {
+          return res.status(400).json({ error: "Siape já cadastrado "});
+        }
+
         const employee = await User.Funcionario.create(req.body);
         return employee;
       }
