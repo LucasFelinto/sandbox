e41d6df9439db1e49940dbcecebff4230aa5244e
Khevinn <51975150+Khevinn@users.noreply.github.com>
2020-03-11 00:37:41 -0300

    Update auth.js
    Só pra ajeitar o nome da variavel

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -2,13 +2,13 @@ import jwt from 'jsonwebtoken';
 import { promisify } from 'util';
 
 export default async (req, res, next) => {
-  const atuhHeader = req.headers.authorization;
+  const authHeader = req.headers.authorization;
 
-  if (!atuhHeader) {
+  if (!authHeader) {
     return res.status(401).json({ error: 'Token não enviado' });
   }
 
-  const [, token] = atuhHeader.split(' ');
+  const [, token] = authHeader.split(' ');
   
   try {
     const decoded = await promisify(jwt.verify)(token, 'ebbf27b2510115c1fcf05b250762a924');
